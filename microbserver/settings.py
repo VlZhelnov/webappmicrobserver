6e1dddfc55bbf8a9282b17b3868e81d2f3d32758
VlZhelnov <vladimir.jelnov2015@yandex.ru>
2020-05-25 23:07:30 +0300

    connect postgres db "atmega" to the project

--- microbserver/settings.py
+++ microbserver/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import keyring
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'microbserver.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+    	'NAME': 'atmega',
+      	'USER': 'vladimir',
+		'PASSWORD': keyring.get_password("atmega", "vladimir"),
+		'HOST': '127.0.0.1',
+		'PORT': '5432',
     }
 }
 
