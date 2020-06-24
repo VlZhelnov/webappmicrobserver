f41764e90cd9677829abe528b39e7b7b990c197c
VlZhelnov <vladimir.jelnov2015@yandex.ru>
2020-06-25 00:40:01 +0300

    changes for synchronization database

--- data_viewer/matplot.py
+++ data_viewer/matplot.py
@@ -1,26 +1,27 @@
 import matplotlib
-from matplotlib.dates import DateFormatter
 from matplotlib.figure import Figure
 
 import urllib, base64
-import datetime, time
-import numpy as np
 import io
 
 def plot_page(Entry, start_time):
 
-	interval = list(start_time + i[0] for i in Entry.values_list('interval'))      
+	interval = Entry.values_list('interval')      
 	temp = Entry.values_list('temperature')
 	light = Entry.values_list('illumination')
 
 	fig = Figure(figsize=(10,10), constrained_layout=True, dpi=100)
 	axs = fig.subplots(2,1)
 
-	axs[0].plot_date(interval, temp, 'r-', xdate=True, tz='Europe/Moscow')
-	axs[1].plot_date(interval, light, 'y-', xdate=True, tz='Europe/Moscow')
-	axs[0].set_title('Ambient temperature') 
+	axs[0].plot(interval, temp, 'r-')
+	axs[1].plot(interval, light, 'y-')
+	axs[0].set_title('Ambient temperature')
 	axs[1].set_title('Degree of illumination')
-      
+	axs[0].set_ylabel('Temperature ($^\circ$C)', color="r")
+	axs[1].set_ylabel('Lighting, %', color="y")
+	axs[0].set_xlabel('time (s)')
+	axs[1].set_xlabel('time (s)')
+        
 	buf = io.BytesIO()
 	fig.savefig(buf, format = 'png')
 	string =  base64.b64encode(buf.getbuffer()).decode("ascii")
--- data_viewer/models.py
+++ data_viewer/models.py
@@ -9,25 +9,28 @@ class Microrequest(models.Model):
 		("completed", "completed"),
 		("error", "error"),
 	]
-	title = models.CharField(max_length=200, db_index=True)
-	data_request = models.DateTimeField(auto_now_add=True)
-	data_accept = models.DateTimeField(blank=True, null=True)
-	data_completed = models.DateTimeField(blank=True, null=True)
-	delay = models.PositiveIntegerField()
-	quantity = models.PositiveIntegerField()
-	status =  models.CharField(max_length=10, choices=STATUS_CHOICES, default = "adopted")
+	title = models.CharField(max_length=200, db_index=True, db_column = "title")
+	data_request = models.DateTimeField(auto_now_add=True, db_column = "data_request")
+	data_accept = models.DateTimeField(blank=True, null=True, db_column = "data_accept")
+	data_completed = models.DateTimeField(blank=True, null=True, db_column = "data_completed")
+	delay = models.PositiveIntegerField(db_column = "delay")
+	quantity = models.PositiveIntegerField(db_column = "quantity")
+	status =  models.CharField(max_length=10, choices=STATUS_CHOICES, default = "adopted", db_column = "status")
 	
 	def get_absolute_url(self):
 	        return reverse('microrequest_detail_url', kwargs={"pk":self.pk})
 	def __str__(self):
 		return self.title
+	class Meta:
+	        db_table = 'microrequest'
 	
 
 class Entry(models.Model):
-	interval = models.DurationField()
-	temperature = models.SmallIntegerField()
-	illumination = models.SmallIntegerField()
-	microrequest =  models.ForeignKey("Microrequest", on_delete=models.CASCADE, related_name="microrequest")
+	interval = models.IntegerField(db_column = "elapsed_time")
+	temperature = models.SmallIntegerField(db_column = "temperature")
+	illumination = models.SmallIntegerField(db_column = "illumination")
+	microrequest =  models.ForeignKey("Microrequest", on_delete=models.CASCADE, related_name="microrequest_id")
 
-	
+	class Meta:
+	        db_table = 'entry'
 
