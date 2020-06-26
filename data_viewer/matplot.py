5bb0960368e70298e63858d2b8edaca9f135cdaa
VlZhelnov <vladimir.jelnov2015@yandex.ru>
2020-06-27 01:18:12 +0300

    upgrade function plot() from matplot.py

--- data_viewer/matplot.py
+++ data_viewer/matplot.py
@@ -1,28 +1,38 @@
-import matplotlib
-from matplotlib.figure import Figure
-
+from matplotlib import pyplot as plt
+from datetime import timedelta as td
+from django.conf import settings
+import pytz
 import urllib, base64
 import io
 
+
+def utc_to_local(utc_dt):
+	local_tz = pytz.timezone(settings.TIME_ZONE)
+	local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+	return local_tz.normalize(local_dt) 
+
 def plot_page(Entry, start_time):
 
-	interval = Entry.values_list('interval')      
+	start_time = utc_to_local(start_time)
+	interval = [start_time + td(seconds = i[0]) for i in Entry.values_list('interval')]
 	temp = Entry.values_list('temperature')
 	light = Entry.values_list('illumination')
-
-	fig = Figure(figsize=(10,10), constrained_layout=True, dpi=100)
-	axs = fig.subplots(2,1)
-
-	axs[0].plot(interval, temp, 'r-')
-	axs[1].plot(interval, light, 'y-')
-	axs[0].set_title('Ambient temperature')
-	axs[1].set_title('Degree of illumination')
-	axs[0].set_ylabel('Temperature ($^\circ$C)', color="r")
-	axs[1].set_ylabel('Lighting, %', color="y")
-	axs[0].set_xlabel('time (s)')
-	axs[1].set_xlabel('time (s)')
-        
+	
+	fig, ax1 = plt.subplots(figsize=(10,5))
+	color = 'tab:red'
+	ax1.set_xlabel('Time (s)')
+	ax1.set_ylabel('Temperature ($^\circ$C)', color=color)
+	ax1.plot_date(interval, temp, xdate=True, fmt="-", color=color)
+	ax1.tick_params(axis='y', labelcolor=color)
+	color = 'tab:purple'
+	ax2 = ax1.twinx() 
+	ax2.set_ylabel('Lighting, %', color=color)
+	ax2.plot_date(interval, light, xdate=True, fmt="-", color=color)
+	ax2.tick_params(axis='y',   labelcolor=color)
+	ax1.grid()
+	fig.tight_layout()
 	buf = io.BytesIO()
 	fig.savefig(buf, format = 'png')
 	string =  base64.b64encode(buf.getbuffer()).decode("ascii")
 	return urllib.parse.quote(string)
+	
